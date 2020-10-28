9e54b825620c6df258709202c5938e698396d4f9
Oliver Cederborg <hi@olivercederborg.com>
2020-10-28 12:32:55 +0100

    commit

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
-const withSass = require('@zeit/next-sass')
-const withPurgeCss = require('next-purgecss')
+const withCss = require("@zeit/next-css");
+const withPurgeCss = require("next-purgecss");
 
-module.exports = withSass(withPurgeCss({
-  target: 'serverless',
-  purgeCssEnabled: ({ dev, isServer }) => !dev && !isServer,
-  purgeCssPaths: [
-    './pages/**/*',
-    './layouts/**/*',
-    './components/**/*',
-  ],
-  purgeCss: {
-    defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    whitelistPatterns: [/^menu/, /^nprogress/, /^page-transition/],
-  },
-}))
+module.exports = withCss(
+   withPurgeCss({
+     purgeCssEnabled: ({ dev, isServer }) => !dev && !isServer, // Only enable PurgeCSS for client-side production builds
+     purgeCssPaths: [
+      "pages/**/*",
+      "components/**/*",
+      "other-components/**/*", // also scan other-components folder
+    ],
+   })
+ );
