2a35daefe812ffa1d048bd756f37a35aac166f9b
Oliver Cederborg <hi@olivercederborg.com>
2020-11-07 20:56:30 +0100

    purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+    require('@fullhuman/postcss-purgecss')({
       content: [
         './pages/**/*.{js,jsx,ts,tsx}',
         './components/**/*.{js,jsx,ts,tsx}'
