96c0c4710a3d25a08ccd19e36ba1a4abe80ca804
Oliver Cederborg <hi@olivercederborg.com>
2020-11-07 21:08:23 +0100

    purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    require('@fullhuman/postcss-purgecss')({
-      content: [
-        './pages/**/*.{js,jsx,ts,tsx}',
-        './components/**/*.{js,jsx,ts,tsx}'
-      ],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    }),
+  "plugins": [
+    'tailwindcss',
+    'autoprefixer',
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: [
+          './pages/**/*.{js,jsx,ts,tsx}',
+          './components/**/*.{js,jsx,ts,tsx}'
+        ],
+        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)+/g) || []
+      }
+    ],
   ]
 }
