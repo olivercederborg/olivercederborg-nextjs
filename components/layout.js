156f8b631d52be8556aa992046980a3e2e55850b
Oliver Cederborg <hi@olivercederborg.com>
2020-11-07 17:51:00 +0100

    scrollspy menus

--- components/layout.js
+++ components/layout.js
@@ -75,7 +75,7 @@ const Layout = ({ children }) => {
 							</AnchorLink>
 						</div>
 						<div className='text-greyText'>
-							<Scrollspy items={['hero', 'work', 'about', 'connect']} currentClassName="is-current" offset={ -350 }>
+							<Scrollspy items={['hero', 'work', 'about', 'connect']} currentClassName="is-current" offset={-350}>
 								<AnchorLink
 									href='#hero'
 									offset='72'
@@ -111,34 +111,37 @@ const Layout = ({ children }) => {
 				</nav>
 			</header>
 
-			<nav
+			{/* <nav
 				className="fixed z-10 w-full md:hidden text-2xl text-center bg-gray-900 bottom-0 bg-opacity-90 flex flex-row justify-evenly text-gray-500 border-t-1 border-gray-800"
+				style={{ "backdropFilter": "saturate(180%) blur(20px)", "WebkitBackdropFilter": "saturate(180%) blur(20px)" }}> */}
+				<Scrollspy items={['hero', 'work', 'about', 'connect']} currentClassName="mobile-current" offset={-350} componentTag={"nav"} className="fixed z-10 w-full md:hidden text-2xl text-center bg-gray-900 bottom-0 bg-opacity-90 flex flex-row justify-evenly text-gray-500 border-t-1 border-gray-800"
 				style={{ "backdropFilter": "saturate(180%) blur(20px)", "WebkitBackdropFilter": "saturate(180%) blur(20px)" }}>
-				<AnchorLink
-					href='#hero'
-					className='px-8 hover:text-white ease-in-out duration-300 py-6'
-				>
-					<BiHomeAlt />
-				</AnchorLink>
-				<AnchorLink
-					href='#work'
-					className='px-8 hover:text-white ease-in-out duration-300 py-6'
-				>
-					<FiBriefcase />
-				</AnchorLink>
-				<AnchorLink
-					href='#about'
-					className='px-8 hover:text-white ease-in-out duration-300 py-6'
-				>
-					<BiUser />
-				</AnchorLink>
-				<AnchorLink
-					href='#connect'
-					className='px-8 hover:text-white ease-in-out duration-300 py-6'
-				>
-					<HiOutlineMail />
-				</AnchorLink>
-			</nav>
+					<AnchorLink
+						href='#hero'
+						className='px-8 hover:text-white ease-in-out duration-300 py-6'
+					>
+						<BiHomeAlt />
+					</AnchorLink>
+					<AnchorLink
+						href='#work'
+						className='px-8 hover:text-white ease-in-out duration-300 py-6'
+					>
+						<FiBriefcase />
+					</AnchorLink>
+					<AnchorLink
+						href='#about'
+						className='px-8 hover:text-white ease-in-out duration-300 py-6'
+					>
+						<BiUser />
+					</AnchorLink>
+					<AnchorLink
+						href='#connect'
+						className='px-8 hover:text-white ease-in-out duration-300 py-6'
+					>
+						<HiOutlineMail />
+					</AnchorLink>
+				</Scrollspy>
+			{/* </nav> */}
 			<main className='font-display'>{children}</main>
 		</div>
 	);
