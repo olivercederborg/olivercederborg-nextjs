77c380d2e8aa1628c8288fcec7c06cb026ed9eef
Oliver Cederborg <hi@olivercederborg.com>
2020-11-17 02:33:15 +0100

    set ga

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,7 +1,7 @@
 import ReactGA from 'react-ga'
 export const initGA = () => {
   console.log('GA init')
-  ReactGA.initialize('UA-xxxxxxxxx-1')
+  ReactGA.initialize('UA-120961215-1')
 }
 export const logPageView = () => {
   console.log(`Logging pageview for ${window.location.pathname}`)
