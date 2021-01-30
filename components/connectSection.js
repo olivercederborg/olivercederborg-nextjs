7729dd66b99c58d60ad54b52c332fb7a185d6ba4
Oliver Cederborg <hi@olivercederborg.com>
2021-01-30 22:10:41 +0100

    contact form added

--- components/connectSection.js
+++ components/connectSection.js
@@ -2,7 +2,9 @@ import { useEffect } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 import { FaTwitter, FaDribbble, FaInstagram, FaLinkedin } from "react-icons/fa";
-import { logEvent } from "../utils/analytics"
+import { logEvent } from "../utils/analytics";
+
+import ContactForm from "./contactForm";
 
 const ConnectSection = (props) => {
    useEffect(() => {
@@ -69,62 +71,64 @@ const ConnectSection = (props) => {
          id='connect'
          className={'pt-26 md:pt-32 pb-14 md:pb-0 overflow-x-hidden' + (props.sectionNumber % 2 === 0 ? ' bg-backgroundTwo ' : ((props.sectionNumber % 2) != 0 ? ' bg-backgroundOne' : ''))}
       >
-         <div className='container pb-36 md:pb-48'>
+         <div className='pb-36 md:pb-48 container'>
             <div className='flex flex-row items-center'>
-               <div className='w-16 h-1 mr-6 section-line bg-primaryBrand'></div>
-               <p className='text-base font-medium section-category text-greyText'>Connect</p>
+               <div className='section-line bg-primaryBrand w-16 h-1 mr-6'></div>
+               <p className='section-category text-greyText text-base font-medium'>Connect</p>
             </div>
 
-            <h2 className='mt-8 text-4xl font-semibold text-white lg:text-secheader'>
+            <h2 className='lg:text-secheader mt-8 text-4xl font-semibold leading-normal text-white'>
                <div className="connect-h2">Interested in working with me</div>
                <div className="connect-h2">or perhaps just talk?</div>
             </h2>
 
-            <p className='mt-6 text-base font-normal leading-loose connect-content text-greyText'>
-               Reach me on social media or by sending an<br></br> email to <a href="mailto:hey@olivercederborg.com?subject=Contact from olivercederborg.com&amp;body=Hey Oliver, I'm contacting you because..." target="_blank" onClick={() => logEvent("Contact me", "Email link clicked")} className="duration-200 ease-in-out text-primaryBrand border-b-1 border-backgroundTwo hover:border-primaryBrand">hey@olivercederborg.com</a>
+            <p className='connect-content text-greyText mt-6 text-base font-normal leading-loose'>
+               Reach me on social media or by sending an<br></br> email to <a href="mailto:hey@olivercederborg.com?subject=Contact from olivercederborg.com&amp;body=Hey Oliver, I'm contacting you because..." target="_blank" onClick={() => logEvent("Contact me", "Email link clicked")} className="text-primaryBrand border-b-1 border-backgroundTwo hover:border-primaryBrand duration-200 ease-in-out">hey@olivercederborg.com</a>
             </p>
 
             <div className="connect-cta">
-               <a
+               {/* <a
                   href="mailto:hey@olivercederborg.com?subject=Contact from olivercederborg.com&amp;body=Hey Oliver, I'm contacting you because..."
                   onClick={() => logEvent("Contact me", "Contact me button clicked")}
-                  className='inline-flex w-auto px-8 py-4 mt-12 text-base font-medium text-white duration-300 ease-in-out bg-primaryBrand hover:bg-primaryGrey'
+                  className='bg-primaryBrand hover:bg-primaryGrey inline-flex w-auto px-8 py-4 mt-12 text-base font-medium text-white duration-300 ease-in-out'
                >
                   Drop an email
-						</a>
+						</a> */}
             </div>
+
+            <ContactForm></ContactForm>
          </div>
-         <footer className='container flex flex-col-reverse items-center justify-between pb-10 text-gray-600 md:flex-row lg:items-center'>
-            <p className='mb-5 duration-300 ease-in-out hover:text-greyText md:mb-0'>
+         <footer className='md:flex-row lg:items-center container flex flex-col-reverse items-center justify-between pb-10 text-gray-600'>
+            <p className='hover:text-greyText md:mb-0 mb-5 duration-300 ease-in-out'>
                &copy; 2020 Oliver Cederborg
 					</p>
 
-            <div className='flex flex-row mb-10 text-white md:mb-0'>
+            <div className='md:mb-0 flex flex-row mb-10 text-white'>
                <a
                   href='https://dribbble.com/oliver'
                   target='_blank'
-                  className='p-4 mx-1 text-xl duration-300 ease-in-out bg-primaryGrey hover:bg-primaryBrand'
+                  className='bg-primaryGrey hover:bg-primaryBrand p-4 mx-1 text-xl duration-300 ease-in-out'
                >
                   <FaDribbble />
                </a>
                <a
                   href='https://twitter.com/olivercederborg'
                   target='_blank'
-                  className='p-4 mx-1 text-xl duration-300 ease-in-out bg-primaryGrey hover:bg-primaryBrand'
+                  className='bg-primaryGrey hover:bg-primaryBrand p-4 mx-1 text-xl duration-300 ease-in-out'
                >
                   <FaTwitter />
                </a>
                <a
                   href='https://instagram.com/olivercederborg'
                   target='_blank'
-                  className='p-4 mx-1 text-xl duration-300 ease-in-out bg-primaryGrey hover:bg-primaryBrand'
+                  className='bg-primaryGrey hover:bg-primaryBrand p-4 mx-1 text-xl duration-300 ease-in-out'
                >
                   <FaInstagram />
                </a>
                <a
                   href='https://www.linkedin.com/in/olivercederborg'
                   target='_blank'
-                  className='p-4 mx-1 text-xl duration-300 ease-in-out bg-primaryGrey hover:bg-primaryBrand'
+                  className='bg-primaryGrey hover:bg-primaryBrand p-4 mx-1 text-xl duration-300 ease-in-out'
                >
                   <FaLinkedin />
                </a>
