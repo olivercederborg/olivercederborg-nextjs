0a6bef4c6e3ce796c883d8ddf0c565487004e34c
Oliver Cederborg <hi@olivercederborg.com>
2021-01-30 22:08:35 +0100

    initial nav fadein only in root

--- components/layout.js
+++ components/layout.js
@@ -1,51 +1,68 @@
 import Head from "next/head";
 import { useEffect } from "react";
+import Link from "next/link";
 import AnchorLink from "react-anchor-link-smooth-scroll";
-import Scrollspy from 'react-scrollspy'
-import { gsap } from 'gsap';
-import { BiHomeAlt, BiUser } from "react-icons/bi"
-import { FiBriefcase } from "react-icons/fi"
-import { HiOutlineMail } from "react-icons/hi"
-import { initGA, logPageView, logEvent } from '../utils/analytics'
+import Scrollspy from "react-scrollspy";
+import { gsap } from "gsap";
+import { BiHomeAlt, BiUser } from "react-icons/bi";
+import { FiBriefcase } from "react-icons/fi";
+import { HiOutlineMail } from "react-icons/hi";
+import { FaDribbble } from "react-icons/fa";
+import { initGA, logPageView, logEvent } from "../utils/analytics";
+import { useRouter } from "next/router";
 
-export const siteTitle = "Oliver Cederborg - Self-taught designer & front-end developer.";
-export const siteKeywords = "oliver cederborg, frontend, front-end, graphic, designer, developer, copenhagen, københavn, denmark, danmark ";
-export const siteDescription = "I'm a 23 year old self-taught designer & front-end developer, focused on details and user experience.";
+export const siteTitle =
+	"Oliver Cederborg - Self-taught designer & front-end developer.";
+export const siteKeywords =
+	"oliver cederborg, frontend, front-end, graphic, designer, developer, copenhagen, københavn, denmark, danmark ";
+export const siteDescription =
+	"I'm a 23 year old self-taught designer & front-end developer, focused on details and user experience.";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, href }) => {
+	const router = useRouter();
 	useEffect(() => {
 		if (!window.GA_INITIALIZED) {
-			initGA()
-			window.GA_INITIALIZED = true
+			initGA();
+			window.GA_INITIALIZED = true;
 		}
-		logPageView()
-		gsap.fromTo("header", 1.5, {
-			y: -30,
-			opacity: 0,
-		}, {
-			y: 0,
-			opacity: 1,
-			delay: 0.2,
-			ease: "power3.inOut"
-		});
+		logPageView();
+		
+		if (router.pathname === "/") {
+			gsap.fromTo(
+				"header",
+				1.5,
+				{
+					y: -30,
+					opacity: 0,
+				},
+				{
+					y: 0,
+					opacity: 1,
+					delay: 0.2,
+					ease: "power3.inOut",
+				}
+			);
+		}
 
 		function onScroll() {
-			const navId = document.getElementById("navigation")
+			const navId = document.getElementById("navigation");
 			const topPos = 250;
 			let currentPosition = window.pageYOffset;
-			if (
-				currentPosition > topPos &&
-				navId.classList.contains("py-12") === true
-			) {
-				navId.classList.add("py-6");
-				navId.classList.remove("py-12");
-			}
-			if (
-				currentPosition < topPos &&
-				navId.classList.contains("py-12") === false
-			) {
-				navId.classList.remove("py-6");
-				navId.classList.add("py-12");
+			if (navId) {
+				if (
+					currentPosition > topPos &&
+					navId.classList.contains("py-12") === true
+				) {
+					navId.classList.add("py-6");
+					navId.classList.remove("py-12");
+				}
+				if (
+					currentPosition < topPos &&
+					navId.classList.contains("py-12") === false
+				) {
+					navId.classList.remove("py-6");
+					navId.classList.add("py-12");
+				}
 			}
 		}
 
@@ -58,101 +75,176 @@ const Layout = ({ children }) => {
 				<title>{siteTitle}</title>
 				<meta name='description' content={siteDescription} />
 				<meta name='keywords' content={siteKeywords} />
-				<meta property='og:title' content="Oliver Cederborg" />
-				<meta property='og:image' content="https://dev.olivercederborg.com/images/preview.png" />
+				<meta property='og:title' content='Oliver Cederborg' />
+				<meta
+					property='og:image'
+					content='https://dev.olivercederborg.com/images/preview.png'
+				/>
 				<meta property='og:description' content={siteDescription} />
-				<meta property='og:type' content="website" />
-				<meta property='og:site_name' content="Oliver Cederborg" />
-				<meta property='og:url' content="https://olivercederborg.com/" />
-				<meta name='twitter:card' content="summary" />
-				<meta name='twitter:site' content="@olivercederborg" />
-				<meta name='twitter:creator' content="@olivercederborg" />
+				<meta property='og:type' content='website' />
+				<meta property='og:site_name' content='Oliver Cederborg' />
+				<meta property='og:url' content='https://olivercederborg.com/' />
+				<meta name='twitter:card' content='summary' />
+				<meta name='twitter:site' content='@olivercederborg' />
+				<meta name='twitter:creator' content='@olivercederborg' />
 				<meta name='twitter:description' content={siteDescription} />
-				<meta name='twitter:image' content="https://dev.olivercederborg.com/images/preview.png" />
+				<meta
+					name='twitter:image'
+					content='https://dev.olivercederborg.com/images/preview.png'
+				/>
 
-				<link rel='icon' sizes="180x180" href='/apple-touch-icon.png' />
-				<link rel='icon' sizes="32x32" href='/favicon_32x32.png' />
-				<link rel='icon' sizes="16x16" href='/favicon_16x16.png' />
+				<link rel='icon' sizes='180x180' href='/apple-touch-icon.png' />
+				<link rel='icon' sizes='32x32' href='/favicon_32x32.png' />
+				<link rel='icon' sizes='16x16' href='/favicon_16x16.png' />
 				<link
 					href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap'
 					rel='stylesheet'
 				></link>
 			</Head>
-			<header className='fixed z-10 hidden w-full bg-backgroundOne border-backgroundTwo md:block'>
-				<nav className='container'>
-					<div
-						id='navigation'
-						className='flex items-center justify-between py-12 font-medium text-white transition-all duration-200 ease-in-out font-display'
-					>
-						<div>
-							<AnchorLink href='#hero'>
-								<img
-									src='/images/logo.svg'
-									alt='Oliver Cederborg Logo'
-									className='h-8 duration-200 ease-in-out hover:opacity-70'
-								/>
-							</AnchorLink>
+			{router.pathname === "/" ? (
+				<header className='bg-backgroundOne border-backgroundTwo md:block fixed z-10 hidden w-full'>
+					<nav className='container'>
+						<div
+							id='navigation'
+							className='font-display flex items-center justify-between py-12 font-medium text-white transition-all duration-200 ease-in-out'
+						>
+							<div>
+								<AnchorLink href='#hero'>
+									<img
+										src='/images/logo.svg'
+										alt='Oliver Cederborg Logo'
+										className='hover:opacity-70 h-8 duration-200 ease-in-out'
+									/>
+								</AnchorLink>
+							</div>
+							<div className='text-greyText'>
+								<Scrollspy
+									items={["hero", "work", "about", "connect"]}
+									currentClassName='is-current'
+									offset={-350}
+								>
+									<AnchorLink
+										href='index#hero'
+										offset='72'
+										className='hover:text-white hidden px-5 duration-300 ease-in-out'
+									>
+										Home
+									</AnchorLink>
+									<AnchorLink
+										href='#work'
+										id='work-link'
+										offset='72'
+										className='hover:text-white px-5 duration-300 ease-in-out'
+									>
+										Work
+									</AnchorLink>
+									<AnchorLink
+										href='#about'
+										offset='72'
+										className='hover:text-white px-5 duration-300 ease-in-out'
+									>
+										About
+									</AnchorLink>
+									<AnchorLink
+										href='#connect'
+										offset='72'
+										className='hover:text-white px-5 duration-300 ease-in-out'
+									>
+										Connect
+									</AnchorLink>
+									<Link href='/portfolio'>
+										<a className='bg-none hover:bg-primaryGrey px-4 py-3 text-white duration-300 ease-in-out'>
+											<FaDribbble className='inline text-lg' />
+										</a>
+									</Link>
+								</Scrollspy>
+							</div>
 						</div>
-						<div className='text-greyText'>
-							<Scrollspy items={['hero', 'work', 'about', 'connect']} currentClassName="is-current" offset={-350}>
-								<AnchorLink
-									href='#hero'
-									offset='72'
-									className='hidden px-5 duration-300 ease-in-out hover:text-white'
+					</nav>
+				</header>
+			) : (
+				<header className='bg-backgroundOne border-backgroundTwo md:block fixed z-10 hidden w-full'>
+					<nav className='container'>
+						<div
+							id='navigation'
+							className='font-display flex items-center justify-between py-12 font-medium text-white transition-all duration-200 ease-in-out'
+						>
+							<div>
+								<Link href='/'>
+									<a>
+										<img
+											src='/images/logo.svg'
+											alt='Oliver Cederborg Logo'
+											className='hover:opacity-70 h-8 duration-200 ease-in-out'
+										/>
+									</a>
+								</Link>
+							</div>
+							<div className='text-greyText'>
+								<Scrollspy
+									items={["hero", "work", "about", "connect"]}
+									currentClassName='is-current'
+									offset={-350}
 								>
-									Home
-							</AnchorLink>
-								<AnchorLink
-									href='#work'
-									id="work-link"
-									offset='72'
-									className='px-5 duration-300 ease-in-out hover:text-white'
-								>
-									Work
-							</AnchorLink>
-								<AnchorLink
-									href='#about'
-									offset='72'
-									className='px-5 duration-300 ease-in-out hover:text-white'
-								>
-									About
-							</AnchorLink>
-								<AnchorLink
-									href='#connect'
-									offset='72'
-									className='px-5 duration-300 ease-in-out hover:text-white'
-								>
-									Connect
-							</AnchorLink>
-							</Scrollspy>
+									<Link href='/#work' id='work-link'>
+										<a className='hover:text-white px-5 duration-300 ease-in-out'>
+											Work
+										</a>
+									</Link>
+									<Link href='/#about'>
+										<a className='hover:text-white px-5 duration-300 ease-in-out'>
+											About
+										</a>
+									</Link>
+									<Link href='/#connect'>
+										<a className='hover:text-white px-5 duration-300 ease-in-out'>
+											Connect
+										</a>
+									</Link>
+									<Link href="">
+										<a className='bg-primaryBrand hover:bg-primaryGrey px-4 py-3 ml-3 text-white duration-300 ease-in-out'>
+											<FaDribbble className='inline text-lg' />
+										</a>
+									</Link>
+								</Scrollspy>
+							</div>
 						</div>
-					</div>
-				</nav>
-			</header>
+					</nav>
+				</header>
+			)}
 
-			<Scrollspy items={['hero', 'work', 'about', 'connect']} currentClassName="mobile-current" offset={-350} componentTag={"nav"} className="fixed bottom-0 z-10 flex flex-row w-full text-2xl text-center text-gray-500 bg-gray-900 border-gray-800 md:hidden bg-opacity-90 justify-evenly border-t-1"
-				style={{ "backdropFilter": "saturate(180%) blur(20px)", "WebkitBackdropFilter": "saturate(180%) blur(20px)" }}>
+			<Scrollspy
+				items={["hero", "work", "about", "connect"]}
+				currentClassName='mobile-current'
+				offset={-350}
+				componentTag={"nav"}
+				className='md:hidden bg-opacity-90 justify-evenly border-t-1 fixed bottom-0 z-10 flex flex-row w-full text-2xl text-center text-gray-500 bg-gray-900 border-gray-800'
+				style={{
+					backdropFilter: "saturate(180%) blur(20px)",
+					WebkitBackdropFilter: "saturate(180%) blur(20px)",
+				}}
+			>
 				<AnchorLink
 					href='#hero'
-					className='px-8 py-6 duration-300 ease-in-out hover:text-white'
+					className='hover:text-white px-8 py-6 duration-300 ease-in-out'
 				>
 					<BiHomeAlt />
 				</AnchorLink>
 				<AnchorLink
 					href='#work'
-					className='px-8 py-6 duration-300 ease-in-out hover:text-white'
+					className='hover:text-white px-8 py-6 duration-300 ease-in-out'
 				>
 					<FiBriefcase />
 				</AnchorLink>
 				<AnchorLink
 					href='#about'
-					className='px-8 py-6 duration-300 ease-in-out hover:text-white'
+					className='hover:text-white px-8 py-6 duration-300 ease-in-out'
 				>
 					<BiUser />
 				</AnchorLink>
 				<AnchorLink
 					href='#connect'
-					className='px-8 py-6 duration-300 ease-in-out hover:text-white'
+					className='hover:text-white px-8 py-6 duration-300 ease-in-out'
 				>
 					<HiOutlineMail />
 				</AnchorLink>
