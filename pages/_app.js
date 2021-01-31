6deb12e42ec60b72b33d734269e5d6e4c75e4867
Oliver Cederborg <hi@olivercederborg.com>
2021-01-31 21:24:06 +0100

    page transisiton

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
-import '../css/style.scss';
-import '../css/tailwind.css';
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { motion, AnimatePresence } from "framer-motion";
+
+import "../css/style.scss";
+import "../css/tailwind.css";
+export default function MyApp({ Component, pageProps, router }) {
+	const spring = {
+		type: "spring",
+		damping: 18,
+		stiffness: 85,
+		when: "afterChildren",
+	};
+	return (
+		<AnimatePresence>
+			<div className='page-transition-wrapper overflow-hidden'>
+				<motion.div
+					transition={spring}
+					key={router.pathname}
+					initial={{ y: -150, opacity: 0 }}
+					animate={{ y: 0, opacity: 1 }}
+					exit={{ y: -150, opacity: 0 }}
+					id='page-transition-container'
+				>
+					<Component {...pageProps} key={router.pathname}  />
+				</motion.div>
+			</div>
+		</AnimatePresence>
+	);
 }
