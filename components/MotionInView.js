173f41f100c8af290fe3cfb043358dc5ffcac2d1
Oliver Cederborg <hi@olivercederborg.com>
2021-02-05 02:52:16 +0100

    Fixed props.

--- components/MotionInView.js
+++ components/MotionInView.js
@@ -2,7 +2,7 @@ import { useInView } from "react-intersection-observer"; // 1.9K gzipped
 import { motion, useAnimation } from "framer-motion";
 import React, { useEffect } from "react";
 
-const MotionInView = (props, { children }) => {
+const MotionInView = ({ children }, props) => {
 	const animation = useAnimation();
 	const [ref, inView, entry] = useInView({ threshold: 0.1 });
 
@@ -21,13 +21,13 @@ const MotionInView = (props, { children }) => {
 			transition: {
 				duration: 0.75,
 				delay: 0.75,
-				staggerChildren: 0.5,
-			},
+				staggerChildren: 0.5
+			}
 		},
 		hidden: {
 			y: -30,
-			opacity: 0,
-		},
+			opacity: 0
+		}
 	};
 
 	return (
@@ -35,7 +35,8 @@ const MotionInView = (props, { children }) => {
 			ref={ref}
 			animate={animation}
 			initial='hidden'
-			variants={props.variants ? props.variants : variants}>
+			variants={props.variants ? props.variants : variants}
+		>
 			{children}
 		</motion.div>
 	);
