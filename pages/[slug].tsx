bad0a0402579036162ef0d788b6c192f9bd5ba6b
Oliver Cederborg <hi@olivercederborg.com>
2021-09-02 23:41:07 +0200

    feat(cms): add dynamic project route

---
+++ pages/[slug].tsx
@@ -0,0 +1,107 @@
+import { sanity } from 'lib/sanity'
+import { groq } from 'next-sanity'
+import { useNextSanityImage } from 'next-sanity-image'
+import BlockContent from '@sanity/block-content-to-react'
+import { SanityImageSource } from '@sanity/image-url/lib/types/types'
+import Image from 'next/image'
+
+import type {
+	GetStaticPaths,
+	GetStaticProps,
+	InferGetStaticPropsType
+} from 'next'
+
+type StaticProps = {
+	project: {
+		title: string
+		slug: string
+		mainImage: SanityImageSource
+		body: BlockContent
+		categories: {
+			title: string
+		}[]
+	}
+}
+
+const projectQuery = (
+	slug: string
+) => groq`*[_type == 'project' && defined(slug.current) && slug.current == '${slug}'][0]{
+  title,
+  "slug": slug.current,
+  mainImage,
+	body,
+	categories[]->{
+		title
+	}
+}`
+
+export const getStaticProps: GetStaticProps<StaticProps> = async ({
+	params
+}) => {
+	const { slug } = params
+
+	const project = await sanity().fetch<StaticProps['project']>(
+		projectQuery(slug.toString())
+	)
+
+	return {
+		props: {
+			project
+		},
+		revalidate: 600
+	}
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+	const projects = await sanity().fetch<{ slug: any }[]>(
+		groq`*[_type == 'project' && defined(slug.current)]{
+      "slug": slug.current
+    }`
+	)
+
+	return {
+		paths: projects.map(({ slug }) => ({
+			params: {
+				slug
+			}
+		})),
+		fallback: 'blocking'
+	}
+}
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>
+
+const ProjectPage = ({ project }: Props) => {
+	const bannerImageProps = useNextSanityImage(sanity(), project.mainImage)
+	console.log(project, bannerImageProps)
+	return (
+		<>
+			<main className='container'>
+				<h1 className='mt-8 text-4xl text-white'>{project.title}</h1>
+				<section className='mt-2 mb-8 space-x-2 text-pink-400'>
+					{project.categories.map((category, i) => (
+						<span key={i}>{category.title}</span>
+					))}
+				</section>
+
+				<Image
+					{...bannerImageProps}
+					alt={project.title}
+					height='500'
+					objectFit='cover'
+					objectPosition='center 75%'
+				/>
+
+				<BlockContent
+					blocks={project.body}
+					className='mt-10 space-y-2 text-white'
+					imageOptions={{ h: 240, fit: 'max' }}
+					projectId='nzfi6lh3'
+					dataset='production'
+				/>
+			</main>
+		</>
+	)
+}
+
+export default ProjectPage
