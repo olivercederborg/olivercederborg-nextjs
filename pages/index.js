10cc97e3decd6cc3cf7fd7e8df128c2cd01e95c7
Oliver Cederborg <hi@olivercederborg.com>
2021-09-02 12:36:38 +0200

    feat(cms): sanity init

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import { useEffect } from "react";
 import { gsap } from "gsap";
 
@@ -16,64 +15,6 @@ const Index = (props) => {
 
 	return (
 		<Layout>
-			<Head>
-				<link
-					rel='preload'
-					as='image'
-					href='/images/hero-graphic.webp'
-					media='(min-width: 768px)'
-				/>
-				<link
-					rel='preload'
-					as='image'
-					href='/images/lunar-background.webp'
-					media='(min-width: 768px)'
-				/>
-				<link
-					rel='preload'
-					as='image'
-					href='/images/lunar-first.webp'
-					media='(min-width: 768px)'
-				/>
-				<link
-					rel='preload'
-					as='image'
-					href='/images/lunar-second.webp'
-					media='(min-width: 768px)'
-				/>
-				{/* <link rel="preload" as="image" href="/images/tablet-background.webp" media="(min-width: 768px)" /> */}
-				<link
-					rel='preload'
-					as='image'
-					href='/images/nordsjaelland-osteopati-case-tablet.webp'
-					media='(min-width: 768px)'
-				/>
-				<link
-					rel='preload'
-					as='image'
-					href='/images/noyer-case-tablet.webp'
-					media='(min-width: 768px)'
-				/>
-
-				<link
-					rel='preload'
-					as='image'
-					href='/images/lunar-way-project-mobile.webp'
-					media='(max-width: 767px)'
-				/>
-				<link
-					rel='preload'
-					as='image'
-					href='/images/noyer-project-mobile.webp'
-					media='(max-width: 767px)'
-				/>
-				<link
-					rel='preload'
-					as='image'
-					href='/images/nordsjaelland-osteopati-project-mobile.webp'
-					media='(max-width: 767px)'
-				/>
-			</Head>
 			<HeroSection />
 			<WorkSection sectionNumber={2} />
 			<AboutSection sectionNumber={3} />
