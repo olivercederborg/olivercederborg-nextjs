6a28ff986d8686172d657c4d7184e8a4be8047bf
Oliver Cederborg <hi@olivercederborg.com>
2021-09-04 02:08:02 +0200

    feat(cms): update category schema

--- cms/schemas/category.js
+++ cms/schemas/category.js
@@ -1,17 +1,13 @@
 export default {
-  name: 'category',
-  title: 'Category',
-  type: 'document',
-  fields: [
-    {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    },
-    {
-      name: 'description',
-      title: 'Description',
-      type: 'text',
-    },
-  ],
+	name: 'category',
+	title: 'Category',
+	type: 'document',
+	fields: [
+		{
+			name: 'title',
+			title: 'Title',
+			type: 'string',
+			initialValue: 'New Category'
+		}
+	]
 }
