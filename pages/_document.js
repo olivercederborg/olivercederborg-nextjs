4f88c6ae99092336ea554a0fd7ed087a1bf647eb
Oliver Cederborg <hi@olivercederborg.com>
2021-09-06 03:34:04 +0200

    refactor: remove old image preloads

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
 	render() {
@@ -11,64 +11,14 @@ class MyDocument extends Document {
 						href='/images/hero-graphic.webp'
 						media='(min-width: 768px)'
 					/>
-					<link
-						rel='preload'
-						as='image'
-						href='/images/lunar-background.webp'
-						media='(min-width: 768px)'
-					/>
-					<link
-						rel='preload'
-						as='image'
-						href='/images/lunar-first.webp'
-						media='(min-width: 768px)'
-					/>
-					<link
-						rel='preload'
-						as='image'
-						href='/images/lunar-second.webp'
-						media='(min-width: 768px)'
-					/>
-					{/* <link rel="preload" as="image" href="/images/tablet-background.webp" media="(min-width: 768px)" /> */}
-					<link
-						rel='preload'
-						as='image'
-						href='/images/nordsjaelland-osteopati-case-tablet.webp'
-						media='(min-width: 768px)'
-					/>
-					<link
-						rel='preload'
-						as='image'
-						href='/images/noyer-case-tablet.webp'
-						media='(min-width: 768px)'
-					/>
-
-					<link
-						rel='preload'
-						as='image'
-						href='/images/lunar-way-project-mobile.webp'
-						media='(max-width: 767px)'
-					/>
-					<link
-						rel='preload'
-						as='image'
-						href='/images/noyer-project-mobile.webp'
-						media='(max-width: 767px)'
-					/>
-					<link
-						rel='preload'
-						as='image'
-						href='/images/nordsjaelland-osteopati-project-mobile.webp'
-						media='(max-width: 767px)'
-					/>
 				</Head>
 				<body>
 					<Main />
 					<NextScript />
 				</body>
 			</Html>
-		);
+		)
 	}
 }
 
-export default MyDocument;
+export default MyDocument
