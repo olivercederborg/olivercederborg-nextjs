778d7b72447eaa7bca50a66df937ab5f206d52ff
Oliver Cederborg <hi@olivercederborg.com>
2021-09-06 03:25:41 +0200

    refactor: remove gsap

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -1,69 +1,22 @@
-import AnchorLink from "react-anchor-link-smooth-scroll";
-import { useEffect } from "react";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import { logEvent } from "../utils/analytics";
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const HeroSection = (props) => {
-	useEffect(() => {
-		if (typeof window !== "undefined") {
-			gsap.registerPlugin(ScrollTrigger);
-		}
-		//hero timeline
-		const titleRevealTl = gsap.timeline({
-			scrollTrigger: {
-				trigger: "#hero-content .reveal-title",
-				start: "top bottom",
-				end: "=-200"
-			}
-		});
-		titleRevealTl
-			.to(".reveal-title-content", 0, {
-				visibility: "visible",
-				delay: 0.3
-			})
-			.to(".reveal-title-revealer", 1, {
-				scaleX: 0,
-				transformOrigin: "right",
-				ease: "power4.inOut",
-				stagger: 0.6
-			})
-			.fromTo(
-				".hero-add",
-				1,
-				{
-					y: -20,
-					opacity: 0
-				},
-				{
-					y: 0,
-					opacity: 1,
-					delay: -0.3,
-					stagger: 0.3,
-					ease: "power3.out"
-				}
-			);
-	});
-
 	return (
 		<section
 			id='hero'
-			className='bg-backgroundOne lg:bg-hero-pattern lg:bg-large-desk xl:bg-none bg-no-repeat'
-		>
+			className='bg-backgroundOne lg:bg-hero-pattern lg:bg-large-desk xl:bg-none bg-no-repeat'>
 			<div className='pb-30 md:pt-56 lg:pt-60 xl:pt-72 lg:pb-56 md:min-h-0 xl:bg-hero-pattern bg-large-desk xl:bg-large-desk-xl container flex min-h-screen pt-10 bg-no-repeat'>
 				<div id='hero-content' className='flex flex-col items-start w-full'>
 					<img
 						src='/images/logo.svg'
 						alt='oliver cederborg logo'
-						className='md:hidden h-8 mb-20'
-					></img>
+						className='md:hidden h-8 mb-20'></img>
 
 					<div>
 						<div className='reveal-title md:mb-4 relative mb-2'>
-							<span className='reveal-title-revealer bg-backgroundOne absolute top-0 bottom-0 z-0 w-full h-full' />
-							<span className='reveal-title-content invisible'>
+							<span className='reveal-title-content '>
 								<h1 className='lg:text-6xl lg:leading-snug md:text-5xl py-2 text-4xl font-semibold leading-normal text-white'>
-									Hey, Im{" "}
+									Hey, Im{' '}
 									<span className='text-primaryBrand'>Oliver</span>
 								</h1>
 							</span>
@@ -71,18 +24,16 @@ const HeroSection = (props) => {
 					</div>
 
 					<div className='reveal-title md:mb-4 relative mb-2'>
-						<span className='reveal-title-revealer bg-backgroundOne absolute top-0 bottom-0 z-0 w-full h-full' />
-						<span className='reveal-title-content invisible'>
+						<span className='reveal-title-content '>
 							<h1 className='lg:text-5xl lg:leading-normal md:text-4xl py-1 text-3xl font-semibold leading-normal text-white'>
-								a <span className='text-primaryBrand'>self-taught</span>{" "}
+								a <span className='text-primaryBrand'>self-taught</span>{' '}
 								designer
 							</h1>
 						</span>
 					</div>
 
 					<div className='reveal-title relative'>
-						<span className='reveal-title-revealer bg-backgroundOne absolute top-0 bottom-0 z-0 w-full h-full' />
-						<span className='reveal-title-content invisible'>
+						<span className='reveal-title-content '>
 							<h1 className='lg:text-5xl lg:leading-normal md:text-4xl py-1 text-3xl font-semibold leading-normal text-white'>
 								&amp; front-end developer.
 							</h1>
@@ -96,18 +47,14 @@ const HeroSection = (props) => {
 						<AnchorLink
 							href='#work'
 							offset={() => 72}
-							onClick={() =>
-								logEvent(`Hero CTA`, `User clicked CTA on hero section`)
-							}
-							className='default-focus bg-primaryBrand hover:bg-lighterGrey md:px-8 mt-14 inline-flex justify-center w-full py-4 text-base font-medium text-center text-white duration-300 ease-in-out'
-						>
+							className='default-focus bg-primaryBrand hover:bg-lighterGrey md:px-8 mt-14 inline-flex justify-center w-full py-4 text-base font-medium text-center text-white duration-300 ease-in-out'>
 							See my work
 						</AnchorLink>
 					</div>
 				</div>
 			</div>
 		</section>
-	);
-};
+	)
+}
 
-export default HeroSection;
+export default HeroSection
